791aad9f0148644870eb6341cfe0143a82c334b0
Vidsneezes <moraleszez@gmail.com>
2017-09-30 21:25:29 -0600

    fixed linter linebreak error

--- src/components/CountriesViewer.vue
+++ src/components/CountriesViewer.vue
@@ -15,34 +15,29 @@
     </ul>
   </div>
 </template>
-
 <script>
 export default {
   name: 'countries',
   data() {
     return {
-      countries: ["Guatemala, El Salvador", "United States of America"],
+      countries: ['Guatemala', 'El Salvador', 'United States of America'],
     };
   },
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
 }
-
 ul {
   list-style-type: none;
   padding: 0;
 }
-
 li {
   display: inline-block;
   margin: 0 10px;
 }
-
 a {
   color: #42b983;
 }
