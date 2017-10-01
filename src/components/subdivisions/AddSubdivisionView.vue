e8cd921e1e230c5ee9c947f2f5515797f41afc59
Vidsneezes <moraleszez@gmail.com>
2017-10-01 14:30:44 -0600

    added style to add subdivision

--- src/components/subdivisions/AddSubdivisionView.vue
+++ src/components/subdivisions/AddSubdivisionView.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="country">
-    <input v-model="name" placeholder="Country Name" type="text"/>
-    <input v-model="code" placeholder="Code" type="text"/>
+    <b-nav fill class="nav-bar">
+      <b-nav-item><b-link :to="{name:'SubdivisionsGrid', params:{countryid: country_id}}">Back To Subdivisions</b-link></b-nav-item>
+    </b-nav> 
+    <b-container class="country-entry-form" style="width: 280px;">
+      <b-row class="country-entry">
+        <b-col>
+          Name: 
+        </b-col>
+        <b-col>
+          <input v-model="name" placeholder="Country Name" type="text"/>
+        </b-col>
+      </b-row>
+      <b-row class="country-entry">
+        <b-col>
+          Code: 
+        </b-col>
+        <b-col>
+          <input v-model="code" placeholder="Code" type="text"/>
+        </b-col>
+      </b-row>
     <button v-on:click='addSubdivision'>Add Subdivision</button>
-    <router-link :to="{name:'SubdivisionsGrid', params:{countryid: country_id}}">Back To Subdivisions</router-link>
+    </b-container>
   </div>
 </template>
 <script>
@@ -41,5 +59,21 @@ li {
 }
 a {
   color: green;
+}
+.nav-bar{
+  background: lightseagreen;
+  padding-bottom: 15px;
+  padding-top: 10px;
+  
+}
+.nav-bar a {
+  color: white;
+}
+.country-entry{
+  margin-bottom: 20px;
+  
+}
+.country-entry-form {
+  margin-top: 30px;
 }
 </style>
