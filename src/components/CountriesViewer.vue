acfce546e5321679991ea0498e5007f1ed5cf50e
Vidsneezes <moraleszez@gmail.com>
2017-09-30 20:54:14 -0600

    added skeletong for countries viewer

---
+++ src/components/CountriesViewer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="countries">
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+    <ul id="countries-list" >
+        <li v-for="country in countries" :key="country">
+            <a href="/" target="_blanck">{{country}}</a>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'countries',
+  data() {
+    return {
+      countries: ["Guatemala, El Salvador", "United States of America"],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
